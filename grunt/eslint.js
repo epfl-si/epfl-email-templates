7c095c982458ab06af15ea5191ae4b78b12b407b
William Belle <william.belle@gmail.com>
2021-05-21 18:35:44 +0200

    Add grunt task eslint

---
+++ grunt/eslint.js
@@ -0,0 +1,13 @@
+/*
+ * (c) ECOLE POLYTECHNIQUE FEDERALE DE LAUSANNE, Switzerland, VPSI, 2021.
+ */
+
+module.exports = {
+  options: {
+    configFile: '.eslintrc.json'
+  },
+  target: [
+    '**/*.js',
+    '!node_modules/**/*.js'
+  ]
+};
---
+++ gruntfile.js
@@ -0,0 +1,7 @@
+/*
+ * (c) ECOLE POLYTECHNIQUE FEDERALE DE LAUSANNE, Switzerland, VPSI, 2021.
+ */
+
+module.exports = function (grunt) {
+  require('load-grunt-config')(grunt, {});
+};
