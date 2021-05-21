f01bd37b170a1bd7485ccbeba1a122dda06ec061
William Belle <william.belle@gmail.com>
2021-05-21 18:46:32 +0200

    Add grunt task serve

---
+++ grunt/connect.js
@@ -0,0 +1,14 @@
+/*
+ * (c) ECOLE POLYTECHNIQUE FEDERALE DE LAUSANNE, Switzerland, VPSI, 2021.
+ */
+
+module.exports = {
+  build: {
+    options: {
+      port: 8200,
+      base: 'www/',
+      hostname: '*',
+      livereload: true
+    }
+  }
+};
---
+++ grunt/watch.js
@@ -0,0 +1,15 @@
+/*
+ * (c) ECOLE POLYTECHNIQUE FEDERALE DE LAUSANNE, Switzerland, VPSI, 2021.
+ */
+
+module.exports = {
+  build: {
+    files: [
+      'src/**/*'
+    ],
+    tasks: ['build']
+  },
+  options: {
+    livereload: true
+  }
+};
