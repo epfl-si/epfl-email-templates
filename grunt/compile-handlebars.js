b23212019a679ae3fda72b5ed5534f3443707df9
William Belle <william.belle@gmail.com>
2021-05-21 19:04:06 +0200

    Add grunt task build

---
+++ grunt/compile-handlebars.js
@@ -0,0 +1,19 @@
+/*
+ * (c) ECOLE POLYTECHNIQUE FEDERALE DE LAUSANNE, Switzerland, VPSI, 2021.
+ */
+
+module.exports = {
+  includes: {
+    files: [{
+      cwd: 'src',
+      expand: true,
+      src: 'pages/*.hbs',
+      dest: 'www/',
+      ext: '.html'
+    }],
+    partials: 'src/partials/**/*.hbs',
+    globals: [{
+      YEAR: (new Date()).getFullYear().toString()
+    }]
+  }
+};
